0cae36de0c413698ba44b071355ecd30f7e945e0
202202608 오태영 <otaeyeong2003@gmail.com>
2025-11-03 22:26:10 +0900

    Update bird.cpp

--- bird.cpp
+++ bird.cpp
@@ -6,16 +6,19 @@ class Duck : public Bird {
 public:
     Duck() : Bird("Duck") {
         cry();}
-
-    void cry() override {
-        cout <<"Quack!"<< endl;}
     ~Duck() override {
         cry();}
+        
+    void cry() override {
+        cout <<"Quack!"<< endl;}
+    
 };
 
 class Penguin : public Bird {
 public:
     Penguin() : Bird("Penguin") {
+        cry();}   
+    ~Penguin() override {
         cry();}
 
     void cry() override {
@@ -24,21 +27,21 @@ public:
     void fly() override {
         cout << "This bird can't fly." << endl;}
 
-    ~Penguin() override {
-        cry();}
+
 };
 
 class Eagle : public Bird {
 public:
     Eagle() : Bird("Eagle") {
         cry();}
-
+    ~Eagle() override {
+        cry();
+}
     void cry() override {
         cout << "Screech!" << endl;}
 
     void swim() override {
         cout << "This bird can't swim." << endl;}
 
-    ~Eagle() override {
-        cry();}
+
 };
